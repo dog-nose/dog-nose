d5ab66d6cea1538351c79e5b1d1ce02412369449
DogNose <y.hiro4823gta+github@gmail.com>
2025-05-30 09:01:33 +0900

    Delete commentout

--- config/my_nvim/init.lua
+++ config/my_nvim/init.lua
@@ -1,20 +1,6 @@
 vim.scriptencoding = "utf-8"
 vim.g.mapleader = " "
 
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not (vim.uv or vim.loop).fs_stat(lazypath) then
---   vim.fn.system({
---     "git",
---     "clone",
---     "--filter=blob:none",
---     "https://github.com/folke/lazy.nvim.git",
---     "--branch=stable", -- latest stable release
---     lazypath,
---   })
--- end
--- vim.opt.rtp:prepend(lazypath)
--- local plugins = require('plugins')
--- require('lazy').setup(plugins)
 require("config.lazy")
 
 local options = {
