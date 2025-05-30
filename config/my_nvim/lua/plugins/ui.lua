99b3dd83841817ee59f27893dbaae18379dd6df6
DogNose <y.hiro4823gta+github@gmail.com>
2025-05-30 09:50:41 +0900

    Fix

--- config/my_nvim/lua/plugins/ui.lua
+++ config/my_nvim/lua/plugins/ui.lua
@@ -9,7 +9,7 @@ return {
 					component_separators = { left = "|", right = "|" },
 					section_separators = { left = "", right = "" },
 					disabled_filetypes = {
-						statusline = {},
+						statusline = {"NvimTree"},
 						winbar = {},
 					},
 					ignore_focus = {},
